80d7ea356f0559b2851032971376426bd8d2201b
Lex Semenenko <lex@lex.me>
2019-04-09 23:06:55 -0400

    Filtering tasks in store

--- src/app.js
+++ src/app.js
@@ -11,6 +11,8 @@ import AppRouter from './routers/AppRouter';
 // =============================================================================
 
 // ADD_TASK
+// =============================================================================
+
 const addTask = ({
   description = '',
   note = '',
@@ -28,21 +30,52 @@ const addTask = ({
 });
 
 // REMOVE_TASK
+// =============================================================================
 const removeTask = ({ id } = {}) => ({
   type: 'REMOVE_TASK',
   id
 });
 
 // EDIT_TASK
-// SET_TEXT_FILTER
-// SORT_BY_DATE
-// SORT_BY_TIME
-// SET_START_DATE
-// SET_END_DATE
+// =============================================================================
+const editTask = (id, updates) => ({
+  type: 'EDIT_TASK',
+  id,
+  updates
+});
 
+// SET_TEXT_FILTER
 // =============================================================================
-// Break application to multiple Reducers
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+});
+
+// SORT_BY_DATE
 // =============================================================================
+const sortByDate = () => ({
+  type: 'SORT_BY_DATE'
+});
+
+// SORT_BY_TIME
+// =============================================================================
+const sortByTime = () => ({
+  type: 'SORT_BY_TIME'
+});
+
+// SET_START_DATE
+// =============================================================================
+const setStartDate = startDate => ({
+  type: 'SET_START_DATE',
+  startDate
+});
+
+// SET_END_DATE
+// =============================================================================
+const setEndDate = endDate => ({
+  type: 'SET_END_DATE',
+  endDate
+});
 
 // Reducer: Tasks
 // =============================================================================
@@ -54,10 +87,21 @@ const tasksReducer = (state = tasksReducerDefaultState, action) => {
       return [...state, action.task];
     case 'REMOVE_TASK':
       return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_TASK':
+      return state.map(task => {
+        if (task.id === action.id) {
+          return {
+            ...task,
+            ...action.updates
+          };
+        }
+        return task;
+      });
     default:
       return state;
   }
 };
+
 // Reducer: Filter
 // =============================================================================
 
@@ -67,18 +111,58 @@ const filtersReducerDefaultState = {
   startDate: undefined,
   endDate: undefined
 };
-
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
+    case 'SORT_BY_TIME':
+      return {
+        ...state,
+        sortBy: 'time'
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
+      };
+    case 'SET_START_DATE':
+      return {
+        ...state,
+        startDate: action.startDate
+      };
+    case 'SET_END_DATE':
+      return {
+        ...state,
+        endDate: action.endDate
+      };
     default:
       return state;
   }
 };
 
+// Get Visible Tasks
+// =============================================================================
+
+const getVisibleTasks = (tasks, { text, sortBy, startDate, endDate }) => {
+  return tasks.filter(task => {
+    const startDateMatch =
+      typeof startDate !== 'number' || task.createdAt >= startDate;
+    const endDateMatch =
+      typeof endDate !== 'number' || task.createdAt <= endDate;
+    const textMatch = task.description
+      .toLowerCase()
+      .includes(text.toLowerCase());
+
+    return startDateMatch && endDateMatch && textMatch;
+  });
+};
+
 // Store Creation
 // =============================================================================
-// tasks: tasksReducer task reducer seting up to be managed by tasksReducer
-//
+
 const store = createStore(
   combineReducers({
     tasks: tasksReducer,
@@ -87,19 +171,43 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-  console.log(store.getState().tasks);
+  const state = store.getState();
+  const visibleTasks = getVisibleTasks(state.tasks, state.filters);
+  console.log(visibleTasks);
+  // console.log(store.getState().filters, 'filters');
+  // console.log(store.getState().tasks, 'tasks');
 });
 
+// =============================================================================
+// Dispatching
+// =============================================================================
+
+// Dispatch: Tasks
+// =============================================================================
+
 const taskOne = store.dispatch(
-  addTask({ description: 'Task one description', time: '1000' })
+  addTask({
+    description: 'Task one description high priority',
+    time: 1000,
+    createdAt: 1000
+  })
 );
 const taskTwo = store.dispatch(
-  addTask({ description: 'Task two description', time: '2000' })
+  addTask({ description: 'Task two description', time: 2000, createdAt: -1000 })
 );
+// store.dispatch(removeTask({ id: taskOne.task.id }));
+// store.dispatch(editTask(taskTwo.task.id, { time: 5000 }));
 
-console.log(taskOne.task.id);
+// Dispatch: Filters
+// =============================================================================
 
-store.dispatch(removeTask({ id: taskOne.task.id }));
+store.dispatch(setTextFilter('high'));
+// store.dispatch(setTextFilter());
+// store.dispatch(sortByTime());
+// store.dispatch(sortByDate());
+// store.dispatch(setStartDate(0));
+// store.dispatch(setStartDate());
+// store.dispatch(setEndDate(999));
 
 const demoState = {
   tasks: [
